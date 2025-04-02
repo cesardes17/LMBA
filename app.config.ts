164fc1ba392149c6c23c7ba7d95f1acff7f0b0d7
César José Delgado Suárez <cjdsgithub@gmail.com>
2025-04-02 20:29:41 +0100

    build: actualizar la configuración del proyecto para la integración de expo-router
    Agregar babel.config.js para dar soporte a expo-router. Actualizar app.json, tsconfig.json, package.json y app.config.ts para incluir las configuraciones y dependencias necesarias para expo-router, formik y otros paquetes relacionados. Estos cambios aseguran la compatibilidad y mejoran la estructura del proyecto para el desarrollo futuro.

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,39 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
 export default {
   expo: {
     name: 'LMBA',
-    // ... other expo config
+    slug: 'LMBA',
+    scheme: 'lmba',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.yourcompany.lmba'
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      },
+      package: 'com.yourcompany.lmba'
+    },
     extra: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     },
-  },
-}
+    plugins: ['expo-router'],
+    experiments: {
+      tsconfigPaths: true,
+      typedRoutes: true
+    },
+  }
+};
